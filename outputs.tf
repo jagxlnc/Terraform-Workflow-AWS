ba84fe1623a49b19d44423e54f2dd1423a863462
JAGDISH KOMAKULA <jagxlnc@gmail.com>
2025-04-18 16:16:27 +0530

    Update outputs for static website and CloudFront

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
 output "bucket_id" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.private_bucket.id
+  value       = aws_s3_bucket.website_bucket.id
 }
 
 output "bucket_arn" {
   description = "The ARN of the bucket"
-  value       = aws_s3_bucket.private_bucket.arn
+  value       = aws_s3_bucket.website_bucket.arn
 }
 
-output "bucket_domain_name" {
-  description = "The bucket domain name"
-  value       = aws_s3_bucket.private_bucket.bucket_domain_name
+output "bucket_website_endpoint" {
+  description = "The website endpoint of the bucket"
+  value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
 
-output "bucket_regional_domain_name" {
-  description = "The bucket region-specific domain name"
-  value       = aws_s3_bucket.private_bucket.bucket_regional_domain_name
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "cloudfront_distribution_domain_name" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "cloudfront_distribution_arn" {
+  description = "The ARN of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.arn
 }
